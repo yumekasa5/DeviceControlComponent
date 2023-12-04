3117bd797540d576c5dcfc6fbbf1ca5692fbdfa7
kaigta22 <kaitomx@gmail.com>
2023-12-05 00:31:46 +0900

    add transaction method

--- MasterThread.cpp
+++ MasterThread.cpp
@@ -1,7 +1,20 @@
 #include "MasterThread.h"
 
-MaterThread::MaterThread(QObject *parent)
+MasterThread::MasterThread(QObject *parent)
     : QObject{parent}
 {
 
+}
+
+void MasterThread::transaction(const QString &portName, int waitTimeout, const QString &request)
+{
+    const QMutexLocker locker(&m_mutex);
+    m_portName = portName;
+    m_waitTimeout = waitTimeout;
+    m_request = request;
+    if(!isRunning()){
+        start();
+    }else{
+        m_cond.wakeOne();
+    }
 }
